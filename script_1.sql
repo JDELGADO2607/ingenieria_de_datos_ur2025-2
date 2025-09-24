4e45a88929b2379a90658d894930e746a2afe463
JDELGADO2607 <jmopenbb@gmail.com>
2025-09-24 08:36:19 -0500

    Add files via upload

--- script_1.sql
+++ script_1.sql
@@ -91,5 +91,56 @@ insert into usuario values ("", "Cliente"),("", "Cliente"),("", "Cliente"),("", 
 
 alter table cliente add idUsuarioFK int, add constraint foreign key(idUsuarioFK) references usuario(idUSuario);
 describe cliente;
+alter table cliente modify  cedulaCliente int not null;
+alter table cliente modify telefonoCliente bigint unique;
+insert into cliente values  (1127941351, 3138488188, "Juan", "Delgado", "KR 71D #64I - 14","blabla", 1), (1127941352, 3138488180, "Joan", "Delgado", "KR 72 #64 - 14","vlavla", 2), (1127941353, 3138488181, "Juan", "Deloda", "KR 73 #67 - 16","blablea", 3), (1127941354, 3138488182, "Jan", "Salgado", "KR 77 #64 - 12","blable", 4), (1127941355, 3138488189, "Juan", "Delgado", "KR 71D #64I - 14","bleebla", 11);
 
-insert into cliente values ("", 1127941351, 3138488188, "Juan", "Delgado", "KR 71D #64I - 14","blabla", 1), ("", 1127941352, 3138488180, "Joan", "Delgado", "KR 72 #64 - 14","vlavla", 2), ("", 1127941353, 3138488181, "Juan", "Deloda", "KR 73 #67 - 16","blable", 3), ("", 1127941354, 3138488182, "Jan", "Salgado", "KR 77 #64 - 12","blable", 4), ("", 1127941355, 3138488189, "Juan", "Delgado", "KR 71D #64I - 14","bleebla", 11)
+alter table mascota change column cedulaCliente cedulaClienteFK int not null;
+
+insert into mascota values ("", 1127941351, "Perro", "Pedro", "Bulldog"), ("", 1127941352, "Perro", "Mauricio", "Husky"), ("", 1127941351, "Perro", "Daniel", "Bulldog"), ("", 1127941351, "Gato", "Pedro", "Negro");
+insert into mascota values ("", 1127941353, "Gato", "Alejandro","Cálico");
+describe vacuna;
+insert into vacuna values ("", "Moquillo", "Antimoquillo", 17), ("", "Diarrea", "Antidiarrea", 17), ("", "Vomito", "AntiVomito", 17), ("", "Tos", "Antitos", 17), ("", "Gripe", "Antigripe", 17);
+describe aplicacionVacuna;
+insert into aplicacionVacuna values ("", 1,1), ("",2,2), ("",3,3), ("",4,4), ("",5,5);
+
+show tables;
+describe producto;
+
+insert into producto values (111, 20.5, "Comida Perro", "Animal"), (112, 20.5, "Comida Gato", "Animal"), (113, 20.5, "Comida Perro", "Purina"), (114, 20.5, "Comida Gato", "Purina"), (115, 20.5, "Dulcecito", "Animal");
+
+describe detalleVentaProducto;
+
+insert into venta values (""), (""), (""), (""), ("");
+
+alter table detalleVentaProducto
+drop foreign key detalleventaproducto_ibfk_2, 
+drop column idProducto, 
+	add column codigoBarProdFK int not null, 
+	add constraint codigoBarProdFK
+		foreign key (codigoBarProdFK) references producto(codigoBarProd);
+        
+insert into detalleVentaProducto values (1, "", 111), (2, "", 112), (3, "", 113), (4, "", 114), (5, "", 115);
+
+describe detalleVentaCliente;
+
+alter table detalleVentaCliente algorithm = inplace, lock = none, change column idVenta idVentaFK int not null, change column cedulaCliente cedulaClienteFK int not null;
+alter table detalleVentaCliente modify column cedulaClienteFK bigint not null; 
+
+insert into detalleVentaCliente values ("", 1127941351, 1, ""), ("", 1127941352, 2, ""), ("", 1127941353, 3, ""), ("", 1127941354, 4, ""), ("", 1127941355, 5, "");
+
+select * from detalleVentaCliente;
+
+# Consultas y condicionales
+
+select idVacuna as "Código Vacuna", nombreEnf as "Nombre de la Enfermedad Asociada", dosisVacuna as "Dosis de la Vacuna" from vacuna;
+
+select * from cliente where apellidoCliente like "Del%";
+select * from cliente where apellidoCliente like "%oda";
+select * from cliente where emailCliente like "%able%";
+select * from cliente where nombreCliente like "J%" and nombreCliente like "%n";
+select * from cliente where idUsuarioFK in (1,2,11);
+select * from cliente where apellidoCliente like "Sal%" and emailCliente like "%bla";
+select * from mascota where cedulaClienteFK in (1,2,5,11) and not nombreMascota = "Perro";
+select * from mascota order by cedulaClienteFK desc;
+select * from mascota group by razaMascota;
